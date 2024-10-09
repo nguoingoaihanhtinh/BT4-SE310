49870c69f7609d03f916a14f668c0fd7068b977b
Kiet Phan <22520688@gm.uit.edu.vn>
2024-10-09 17:52:21 +0700

    first commit

---
+++ WebQuanLySanPham/Controllers/CatalogController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using WebQuanLySanPham.Models;
+
+namespace WebQuanLySanPham.Controllers
+{
+    public class CatalogController : Controller
+    {
+        // GET: Catalog
+        public ActionResult Index()
+        {
+            ProductDataContext context = new ProductDataContext("Server=KHOAPC;Database=Product;User Id=sa;Password=1235;");
+            List<Catalog> dsCatalog = context.Catalogs.ToList(); // Ensure this returns a valid list
+
+            // Check if dsCatalog is null
+            if (dsCatalog == null)
+            {
+                // Log the error or handle it accordingly
+                return View("Error"); // Redirect to an error view if necessary
+            }
+
+            return View(dsCatalog);
+        }
+
+    }
+}
